d186ffaa3f2a71f833358d124f2cafb8bf981080
Petúnia Dias <147607484+petuniadias@users.noreply.github.com>
2024-05-23 12:44:01 +0100

    events

--- codebuddy/events.js
+++ codebuddy/events.js
@@ -1,90 +1,44 @@
-const slideShowConf = {
-    "visible": 2,
-    "offset": 60 // =60px best with 40 ;-)
-}
-
-let savedPosition = 0;
-
-function showSlide(element, position) {
-
-    if (element.classList.contains('visible')) {
+function nextContent(currentTarget) {
+    const slide = currentTarget.closest(".slide");
+    const contents = slide.querySelectorAll('.content');
+    if (contents.length == 1) {
         return;
     }
 
-    const slides = element.parentNode.querySelectorAll('.slide');
-    for (const slide of slides) {
-        slide.classList.remove('visible');
-    }
-
-    if (position < savedPosition) {
-        position += (slideShowConf['visible'] - 1);
-    }
-
-    // add class visible to slides in viewport
-    for (let i = 1; i <= slideShowConf['visible']; i++) {
-        slides[position-i].classList.add('visible');
-    }
-
-    const margin = (() => {
-        if (position > slideShowConf['visible']) {
-            const currentWidth = parseInt(element.parentNode.style.width, 10);
-            return (((currentWidth / slides.length) * (position - slideShowConf['visible'])) - slideShowConf['offset']) * -1;
+    let active = 0;
+    for (let i = 0; i < contents.length; i++) {
+        if (contents[i].classList.contains('desacative')) {
+            contents[i].classList.remove('desacative');
         }
-        return slideShowConf['offset'];
-    })();
 
-    element.parentNode.style.marginLeft = `${margin}px`;
-    element.parentNode.style.transition = "all 1s ease-in-out";
+        if (contents[i].classList.contains('active')) {
+            active = i;
+            contents[i].classList.remove('active');
+            contents[i].classList.add('desacative');
+        }
+    }
 
-    savedPosition = position;
+    active++;
+    if (active == contents.length) {
+        active = 0;
+    }
+
+    setTimeout(() => {
+        contents[active].classList.add('active');
+    }, 500);
 }
 
-function slideShowManager(resize = false) {
+function projectManager() {
 
-    if (slideShowConf['visible'] === 0) {
-        console.error('At least 1 slides are required!');
-        return;
-    }
+    const projects = document.getElementById("project-show");
 
-    savedPosition = 0;
-
-    const viewportWidth = window.innerWidth;
-    const slideshow = document.getElementById("slideshow-container");
-
-    const rows = slideshow.querySelectorAll('.row');
-    for (const row of rows) {
-        const slides = row.querySelectorAll('.slide');
-        if (slides.length < slideShowConf['visible']) {
-            console.error(`At least ${slideShowConf['visible']} slides are required!`);
-            break;
-        }
-
-        // total width of row with n visible slides without scroll
-        const innerRowWidth = (((document.body.clientWidth - slideShowConf['offset'] * 2) / slideShowConf['visible']) * slides.length);
-        row.style.width = `${innerRowWidth}px`;
-        row.style.marginLeft = `${slideShowConf['offset']}px`;
-
-        let count = 1;
-        for (const slide of slides) {
-            if (!resize) {
-                slide.addEventListener('click', (event) => {
-                    const position = parseInt(event.currentTarget.dataset.id, 10);
-                    showSlide(event.currentTarget, position);
-                });
-            }
-
-            if (count <= slideShowConf['visible']) {
-                slide.classList.add('visible');
-            } else {
-                slide.classList.remove('visible');
-            }
-            count++;
-        }
+    const arrows = projects.querySelectorAll('.loop-arrow');
+    for (const arrow of arrows) {
+        arrow.addEventListener('click', (event) => {
+            nextContent(event.currentTarget);
+        });
+        arrow.parentElement.children[0].classList.add('active');
     }
 }
 
-window.addEventListener("resize", (event) => {
-    slideShowManager(true);
-});
-
-slideShowManager();
+projectManager();
